1ab48ddc187a50c99f588746f4f8a23badf1199a
Benjamin Atbi <benjaminatbi@gmail.com>
2022-01-28 16:44:16 -0800

    finished part 2 and 3

--- googletest-template/test/Testcases.cpp
+++ googletest-template/test/Testcases.cpp
@@ -1,6 +1,9 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "Parallelogram.h"
+#include "Matthews.h"
+#include "Awards.h"
+#include <string>
 
 using namespace testing;
 
@@ -39,3 +42,60 @@ TEST_F(ParallelogramTests, getKindPrecedence) {
     EXPECT_EQ(p_square->getKind(), shapes::Parallelogram::Kind::SQUARE);
 }
 
+////////////
+
+TEST(MatthewsTests, reachZero) {
+    EXPECT_EQ(sequence::checkMatthewsOutcome(3), sequence::MatthewsOutcome::ZERO);
+    EXPECT_EQ(sequence::checkMatthewsOutcome(4), sequence::MatthewsOutcome::ZERO);
+    EXPECT_EQ(sequence::checkMatthewsOutcome(5), sequence::MatthewsOutcome::ZERO);
+}
+
+TEST(MatthewsTests, reachMinusOneCycle) {
+    EXPECT_EQ(sequence::checkMatthewsOutcome(-1), sequence::MatthewsOutcome::MINUS_ONE_CYCLE);
+}
+
+TEST(MatthewsTests, reachMinusTwoCycle) {
+    EXPECT_EQ(sequence::checkMatthewsOutcome(-2), sequence::MatthewsOutcome::MINUS_TWO_CYCLE);
+    EXPECT_EQ(sequence::checkMatthewsOutcome(-4), sequence::MatthewsOutcome::MINUS_TWO_CYCLE);
+}
+
+////////////
+
+class RankListStub : public awards::RankList {
+    public:
+
+        RankListStub() { callnum = 0; }
+
+        std::string getNext() {
+            std::string name = "participant" + std::to_string(callnum);
+            callnum++;
+            return name;
+        } 
+    private:
+        int callnum;
+};
+
+class MockAwardCeremonyActions : public awards::AwardCeremonyActions {
+    public:
+        MOCK_METHOD(void, playAnthem, (), (override));
+        MOCK_METHOD(void, turnOffTheLightsAndGoHome, (), (override));
+        MOCK_METHOD(void, awardBronze, (std::string recipient), (override));
+        MOCK_METHOD(void, awardSilver, (std::string recipient), (override));
+        MOCK_METHOD(void, awardGold, (std::string recipient), (override));
+};
+
+TEST(AwardsTests, testCeremony) {
+    MockAwardCeremonyActions ceremony;
+    RankListStub participants;
+
+    {
+        InSequence seq;
+        EXPECT_CALL(ceremony, playAnthem()).Times(1);
+        EXPECT_CALL(ceremony, awardBronze("participant0")).Times(1);
+        EXPECT_CALL(ceremony, awardSilver("participant1")).Times(1);
+        EXPECT_CALL(ceremony, awardGold("participant2")).Times(1);
+        EXPECT_CALL(ceremony, turnOffTheLightsAndGoHome()).Times(1);
+    }
+    
+    awards::performAwardCeremony(participants, ceremony);
+}
